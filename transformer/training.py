df06b2159012459d910cec6743d8d945f8a08baa
sarthakg004 <saarthak.gupta.mec22@itbhu.ac.in>
2024-10-12 09:11:57 +0530

    created training loop

--- transformer/training.py
+++ transformer/training.py
@@ -4,11 +4,19 @@ from torch.utils.data import DataLoader, Dataset, random_split
 
 from dataset import BilingualDataset, casual_mask
 from model import build_transformer
+from config import get_weights_file_path , get_config
+
+from tqdm import tqdm
+
+import warnings
+
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
+from torch.utils.tensorboard import SummaryWriter
+
 from pathlib import Path
 
 def get_all_sentences(ds,lang):
@@ -64,6 +72,85 @@ def get_model(config, vocab_src_size, vocab_tgt_size):
     model = build_transformer(vocab_src_size, vocab_tgt_size, config['seq_len'], config['seq_len'], config['d_model'])
     return model
 
+
+def train_model(config):
+    ## Define the device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Device:', device)
+    
+    Path(config['model_folder']).mkdir(parents = True, exist_ok=True)
+    
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
+    
+    ## TensorBoard  Allows to visualize the training process
+    writer  = SummaryWriter(config['experiment_name'])
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'],eps = 1e-9)
+    
+    ## if there is any error during training we can resume from the last checkpoint
+    initial_epoch = 0
+    global_step = 0
+    
+    if config['preload'] is not None:
+        model_filename = get_weights_file_path(config, config['preload'])
+        print('Loading model weights from:', model_filename)
+        state = torch.load(model_filename)
+        initial_epoch = state['epoch']+1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+    
+    
+    loss_fn = nn.CrossEntropyLoss(ignore_index = tokenizer_tgt.token_to_id("[PAD]"),label_smoothing=0.1)
+    
+    
+    for epoch in range(initial_epoch, config['num_epochs']):
+        model.train()
+        batch_iterator = tqdm(train_dataloader, desc=f'Epoch {epoch}')
+        for batch in batch_iterator:
+            encoder_input = batch['encoder_input'].to(device) ## (Batch, seq_len)
+            decoder_input = batch['decoder_input'].to(device) ## (Batch, seq_len)
+            encoder_mask = batch['encoder_mask'].to(device)  ## (Batch, 1,1, seq_len)
+            decoder_mask = batch['decoder_mask'].to(device)  ## (Batch, 1, seq_len, seq_len)
+            
+            ## Run the tensor through tranformer
+            encoder_output = model.encode(encoder_input, encoder_mask)  ## (Batch, seq_len, d_model)
+            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask)  ## (Batch, seq_len, d_model)
+            proj_output = model.project(decoder_output)  ## (Batch, seq_len, tgt_vocab_size)
+
+            label = batch['label'].to(device) ## (Batch, seq_len)
+            
+            ## (Batch, seq_len, tgt_vocab_size) -> (Batch*seq_len, tgt_vocab_size)
+            loss = loss_fn(proj_output.view(-1, proj_output.size(-1)), label.view(-1))
+            batch_iterator.set_postfix(f("loss":f"{loss.item():6.3f}"))
+            
+            ## Log the loss
+            writer.add_scalar('Train Loss', loss.item(), global_step)
+            writer.flush()
+            
+            ## Backpropagation
+            loss.backward()
+            
+            ## Update the weights
+            optimizer.step()
+            optimizer.zero_grad()
+            
+            global_step += 1
+        
+        ## Save the model weights
+        model_filename = get_weights_file_path(config,f'{epoch:02d}')
+        
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'global_step': global_step,
+        }, model_filename)
+ 
+if __name__ == __main__:
+    warnings.filterwarnings("ignore")
+    config = get_config()
+    train_model(config)
     
     
     
